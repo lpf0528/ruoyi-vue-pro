89837c8e356427f5a945c64f75f7cd829d796237
01Coder <lpf940528@gmail.com>
2026-06-07 22:24:55 +0800

    fix

--- yudao-module-curtain/src/main/java/cn/iocoder/yudao/module/zc/dal/dataobject/workshopuser/ZcWorkshopUserDO.java
+++ yudao-module-curtain/src/main/java/cn/iocoder/yudao/module/zc/dal/dataobject/workshopuser/ZcWorkshopUserDO.java
@@ -13,7 +13,7 @@ import cn.iocoder.yudao.framework.mybatis.core.dataobject.BaseDO;
  *
  * @author 01Coder
  */
-@TableName("zc_workshop_user")
+@TableName(value = "zc_workshop_user", autoResultMap = true)
 @KeySequence("zc_workshop_user_seq") // 用于 Oracle、PostgreSQL、Kingbase、DB2、H2 数据库的主键自增。如果是 MySQL 等数据库，可不写。
 @Data
 @EqualsAndHashCode(callSuper = true)
