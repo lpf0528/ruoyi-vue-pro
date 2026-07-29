fff20b7878d129e89ed6a4b390b7f06d69bba481
01Coder <lpf940528@gmail.com>
2026-07-29 19:29:42 +0800

    节点关联组件

--- yudao-module-curtain/src/main/java/cn/iocoder/yudao/module/zc/dal/mysql/processnode/ZcProcessNodeElementMapper.java
+++ yudao-module-curtain/src/main/java/cn/iocoder/yudao/module/zc/dal/mysql/processnode/ZcProcessNodeElementMapper.java
@@ -5,7 +5,9 @@ import java.util.*;
 import cn.iocoder.yudao.framework.mybatis.core.mapper.BaseMapperX;
 import cn.iocoder.yudao.framework.mybatis.core.query.LambdaQueryWrapperX;
 import cn.iocoder.yudao.module.zc.dal.dataobject.processnode.ZcProcessNodeElementDO;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * 工序节点-关联组件 Mapper
@@ -18,12 +20,14 @@ public interface ZcProcessNodeElementMapper extends BaseMapperX<ZcProcessNodeEle
     /**
      * 按节点ID硬删除关联组件
      *
+     * <p>zc_process_node_element 无 deleted 逻辑删除列，且业务上每次更新都需要彻底清空旧配置，
+     * 因此用原生 SQL 物理删除，绕过 MyBatis Plus 对 {@link ZcProcessNodeElementDO}（继承自 BaseDO，
+     * 标注了 {@code @TableLogic}）默认将 delete 改写为逻辑删除 UPDATE 的行为。</p>
+     *
      * @param processNodeId 节点ID
      */
-    default void deleteByProcessNodeId(Long processNodeId) {
-        delete(new LambdaQueryWrapperX<ZcProcessNodeElementDO>()
-                .eq(ZcProcessNodeElementDO::getProcessNodeId, processNodeId));
-    }
+    @Delete("DELETE FROM zc_process_node_element WHERE process_node_id = #{processNodeId}")
+    void deleteByProcessNodeId(@Param("processNodeId") Long processNodeId);
 
     /**
      * 查询指定节点关联的组件列表
